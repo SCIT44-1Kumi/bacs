ff4c0a4b9e66e478dbd44e8433e15177999a6d1d
Hwikun <leah8608@gmail.com>
2023-08-17 16:14:09 +0900

    init

---
+++ src/main/java/net/softsociety/bacs/security/WebSecurityConfig.java
@@ -0,0 +1,2 @@
+package net.softsociety.bacs.security;public class WebSecurityConfig {
+}
---
+++ src/test/java/net/softsociety/bacs/BacsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.softsociety.bacs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
