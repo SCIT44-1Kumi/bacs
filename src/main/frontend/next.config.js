58a1971cde9242fed1d64afa9918097ce2825fba
Hwikun <leah8608@gmail.com>
2023-08-24 16:19:33 +0900

    next.js API설정
    Signed-off-by: Hwikun <leah8608@gmail.com>

--- src/main/frontend/next.config.js
+++ src/main/frontend/next.config.js
@@ -3,6 +3,15 @@ const { i18n } = require('./next-i18next.config')
 const nextConfig = {
   reactStrictMode: true,
   i18n,
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "http://localhost:8888/bacs/:path*",
+      },
+    ];
+  },
+
 }
 
 module.exports = nextConfig;
