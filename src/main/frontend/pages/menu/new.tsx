aa446c512f2cff0d064c5568ae41a906a6aec777
Hwikun <leah8608@gmail.com>
2023-08-25 19:48:28 +0900

    메뉴 추가 화면 프론트엔드 작업 2차 완료
    Signed-off-by: Hwikun <leah8608@gmail.com>

--- src/main/frontend/pages/menu/new.tsx
+++ src/main/frontend/pages/menu/new.tsx
@@ -1,30 +1,36 @@
 
 import axios from "axios";
-import { useForm } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
 import { useRouter } from "next/router";
-import { useState } from "react";
+
+type Option = {
+	optionNum: number;
+	optionName: string,
+	optionValue: string,
+	optionPrice: number,
+}
 
 type ICreateMenu = {
 	menuName: string,
 	menuPrice: number,
 	menuImg: string,
 	menuDesc: string,
-	optionName: string,
-	optionValue: string,
-	optionPrice: number,
+	options: Option[],
 }
 const CreateMenuForm = () => {
-	const { register, handleSubmit } = useForm<ICreateMenu>();
+	const { control, register, handleSubmit } = useForm<ICreateMenu>();
+	const { fields, append, remove } = useFieldArray({
+		control,
+		name: 'options', // 폼 필드 배열의 이름
+	});
 	const router = useRouter();
-	const [options, setOptions] = useState<number>(0);
 	const onSubmit = async (data: ICreateMenu) => {
+		console.log(data);
 		await axios.post('/menu/menuForm', {data});
 		await router.push("/");
 		return;
 	}
-	const onClickAddOption = () => {
-		setOptions(perv => perv + 1);
-	}
+
 	return <div>
 		<form onSubmit={handleSubmit(onSubmit)} className={`grid grid-cols-2 place-content-center w-1/2 gap-y-4 `}>
 			<label htmlFor="menuName">Name</label>
@@ -35,19 +41,32 @@ const CreateMenuForm = () => {
 			<input className={`border border-black bg-gray-100 rounded-md`} id={`menuImg`} type="string" {...register("menuImg", { required: false })}/>
 			<label htmlFor="menuDesc">Desc</label>
 			<input className={`border border-black bg-gray-100 rounded-md`} id={`menuDesc`} type="string" {...register("menuDesc", { required: false })}/>
-			<input className={`bg-blue-400 rounded-md w-1/2`} onClick={onClickAddOption} id={`addOption`} type="button" value="메뉴옵션 추가" />
-			{options != 0 && [...Array(options)].map((n, index) => {
-				return ( 
-					<div key={index} className={`col-span-2 grid grid-cols-2 gap-2 place-items-center border border-gray-300 rounded-md`}>
-						<label htmlFor={`optionName`}>optionName</label>
-						<input className={`bg-gray-100 rounded-md border-black border`} id={`optionName`} {...register("optionName", { required: false })} type="text" />
-						<label htmlFor={`optionValue`}>optionValue</label>
-						<input className={`bg-gray-100 rounded-md border-black border`} id={`optionValue`} {...register("optionValue", { required: false })} type="text" />
-						<label htmlFor={`optionValue`}>optionPrice</label>
-						<input className={`bg-gray-100 rounded-md border-black border`} id={`optionPrice`} {...register("optionPrice", { required: false })} type="number" />
-					</div>
-				)
-			})}
+			<button type="button" onClick={() => append({ optionNum: 0, optionName: '', optionValue: '', optionPrice: 0})}>
+				Add Option
+			</button>
+			{fields && fields.map((field, index) => (
+				<div key={field.id} className={`col-span-2 grid grid-cols-2 gap-2 place-items-center border border-gray-300 rounded-md`}>
+					<label htmlFor={`optionName`}>optionName</label>
+					<input className={`bg-gray-100 rounded-md border-black border`} id={`optionName`} {...register(`options.${index}.optionName`, { required: true})} type="text" />
+					<label htmlFor={`optionValue`}>optionValue</label>
+					<input className={`bg-gray-100 rounded-md border-black border`} id={`optionValue`} {...register(`options.${index}.optionValue`, { required: true})} type="text" />
+					<label htmlFor={`optionValue`}>optionPrice</label>
+					<input className={`bg-gray-100 rounded-md border-black border`} id={`optionPrice`} {...register(`options.${index}.optionPrice`, { required: true})} type="number" />
+					<input type="hidden" {...register(`options.${index}.optionNum`)} />
+					<button className={`bg-red-400 rounded-md text-white px-4`} type="button" onClick={() => remove(index)}>
+						Remove
+					</button>
+				</div>
+			))}
+			{/*<div key={index} className={`col-span-2 grid grid-cols-2 gap-2 place-items-center border border-gray-300 rounded-md`}>*/}
+			{/*	<label htmlFor={`optionName`}>optionName</label>*/}
+			{/*	<input className={`bg-gray-100 rounded-md border-black border`} id={`optionName`} {...register("optionName", { required: false })} type="text" />*/}
+			{/*	<label htmlFor={`optionValue`}>optionValue</label>*/}
+			{/*	<input className={`bg-gray-100 rounded-md border-black border`} id={`optionValue`} {...register("optionValue", { required: false })} type="text" />*/}
+			{/*	<label htmlFor={`optionValue`}>optionPrice</label>*/}
+			{/*	<input className={`bg-gray-100 rounded-md border-black border`} id={`optionPrice`} {...register("optionPrice", { required: false })} type="number" />*/}
+			{/*	<input type="hidden" {...register("optionNum", { required: false })} />*/}
+			{/*</div>*/}
 			<input className={`bg-blue-400 rounded-md w-1/2`} id={`submit`} type="submit" value="저장" />
 		</form>
 	</div>
