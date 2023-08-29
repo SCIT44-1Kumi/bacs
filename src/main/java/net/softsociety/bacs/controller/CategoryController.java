269ff1c065c53a7de40264f7a95f97e952e3d23b
Hwikun <leah8608@gmail.com>
2023-08-29 16:22:10 +0900

    카테고리 컨트롤러 생성 및 필요한 컨트롤러 생성(TODO포함)
    Signed-off-by: Hwikun <leah8608@gmail.com>

---
+++ src/main/java/net/softsociety/bacs/controller/CategoryController.java
@@ -0,0 +1,19 @@
+package net.softsociety.bacs.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import net.softsociety.bacs.domain.BacsCategory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("category")
+@Slf4j
+public class CategoryController {
+
+
+    // TODO: 카테고리 삭제
+    // TODO: 카테고리 편집
+    // TODO: 카테고리 리스트조회 where 매장아이디
+}
