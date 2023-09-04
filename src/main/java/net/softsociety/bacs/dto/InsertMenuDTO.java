0b1c7c76d5f2dcaa94f803a4cf319215f9035a94
Hwikun <leah8608@gmail.com>
2023-09-04 18:05:04 +0900

    음성인식 기능 1차 커밋
    Signed-off-by: Hwikun <leah8608@gmail.com>

--- src/main/java/net/softsociety/bacs/dto/InsertMenuDTO.java
+++ src/main/java/net/softsociety/bacs/dto/InsertMenuDTO.java
@@ -1,6 +1,7 @@
 package net.softsociety.bacs.dto;
 
 import lombok.Data;
+import net.softsociety.bacs.domain.BacsCategory;
 import net.softsociety.bacs.domain.BacsMenu;
 import net.softsociety.bacs.domain.BacsMenuOption;
 
@@ -8,6 +9,7 @@ import java.util.ArrayList;
 
 @Data
 public class InsertMenuDTO {
+    private BacsCategory category;
     private BacsMenu menu;
     private ArrayList<BacsMenuOption> options;
 }
