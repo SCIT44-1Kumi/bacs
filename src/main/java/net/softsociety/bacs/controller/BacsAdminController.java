6e87b7c8822a9c1f41e8350e5eb59c28724bcf43
Hwikun <leah8608@gmail.com>
2023-09-07 17:28:17 +0900

    관리자 기능 매장 전체 조회 기능

--- src/main/java/net/softsociety/bacs/controller/BacsAdminController.java
+++ src/main/java/net/softsociety/bacs/controller/BacsAdminController.java
@@ -2,12 +2,13 @@ package net.softsociety.bacs.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import net.softsociety.bacs.domain.dto.SaleTodayDTO;
+import net.softsociety.bacs.domain.vo.BacsStore;
+import net.softsociety.bacs.service.AdminService;
 import net.softsociety.bacs.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @Slf4j
@@ -17,9 +18,18 @@ public class BacsAdminController {
     @Autowired
     UserService service;
 
+    @Autowired
+    AdminService adminService;
+
     // TODO: 매장 조회 /store/list
+    @GetMapping("store/list")
+    public ArrayList<BacsStore> getStoreList() {
+        return adminService.getStoreList();
+    }
     // TODO: 매장삭제 /store/delete
     // TODO: 매장차단 /store/block
+
+
 
     /**
      * 당일 매출 조회
