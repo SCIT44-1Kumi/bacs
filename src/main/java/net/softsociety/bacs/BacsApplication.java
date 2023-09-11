7ed9c0a8366c59115acce501de9901c6cb36ef19
Hwikun <leah8608@gmail.com>
2023-09-12 08:24:30 +0900

    application.properties -> application.yml & 기타 앱 설정 변경

--- src/main/java/net/softsociety/bacs/BacsApplication.java
+++ src/main/java/net/softsociety/bacs/BacsApplication.java
@@ -2,8 +2,10 @@ package net.softsociety.bacs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan(basePackages = "net.softsociety.bacs")
 public class BacsApplication {
 
     public static void main(String[] args) {
