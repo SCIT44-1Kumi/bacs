f496b789fc71b5ef3c4610a42cc3e3ea7e77ae0f
Hwikun <leah8608@gmail.com>
2023-11-21 19:08:38 +0900

    최종

--- src/main/frontend/components/kiosk/kioskBG.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentTypes } from "@/pages/_app";
-
-type KioskBGTypes = ComponentTypes & {
-	color: string;
-};
-const KioskBG = ({ color, children }: KioskBGTypes) => {
-	return <div className={`${color} grid grid-cols-1 h-screen`}>{children}</div>;
-};
-export default KioskBG;
